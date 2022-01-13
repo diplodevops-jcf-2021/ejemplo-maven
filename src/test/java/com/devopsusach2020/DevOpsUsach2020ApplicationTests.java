288fa7f1350852c14a889127bae5d8c083275287
Javier Contreras Ferrada <j.contrerasferrada@gmail.com>
2022-01-13 13:55:53 -0300

    agrega test para taller 7 módulo 3

--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -1,13 +1,19 @@
 package com.devopsusach2020;
 
+import com.devopsusach2020.rest.RestData;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootTest
 class DevOpsUsach2020ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private RestData controller;
 
+	@Test
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
 }
